125951ef92e9d06ad9097f64c0cabecdf860f3ab
nicholesales <snichole.0714@gmail.com>
2024-04-14 18:11:57 +0800

    changed sliding button in MechanicPage to normal button and added alert dialog for confirmation of payment

--- app/src/main/java/com/example/bat/MechanicPage.java
+++ app/src/main/java/com/example/bat/MechanicPage.java
@@ -1,8 +1,13 @@
 package com.example.bat;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +15,46 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MechanicPage extends AppCompatActivity {
 
+    Button release;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mechanic_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        release = findViewById(R.id.payBtn);
+
+        release.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MechanicPage.this);
+                // Set the message show for the Alert time
+                builder.setMessage("Confirm release of payment to Mechanic?\nNote: Only confirm once service is done");
+                // Set Alert Title
+                builder.setTitle("Payment Release Confirmation");
+                // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+                builder.setCancelable(false);
+                // Set the positive button with yes name Lambda OnClickListener method is use of DialogInterface interface.
+                builder.setPositiveButton("Confirm", (DialogInterface.OnClickListener) (dialog, which) -> {// When the user click yes button then app will close
+                    Intent intent = new Intent(getApplicationContext(), EndScreen.class);
+                    startActivity(intent);
+                });
+                // Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+                builder.setNegativeButton("Cancel", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                // Create the Alert dialog
+                AlertDialog alertDialog = builder.create();
+                // Show the Alert Dialog box
+                alertDialog.show();
+
+            }
         });
+
+
+
+
     }
 }
